63225671427fc8f47022b024f3e6276c10423ca7
Nathan Chou <starrynte@gmail.com>
2014-05-18 07:02:14 -0700

    Review controller

--- app/routes.php
+++ app/routes.php
@@ -27,11 +27,6 @@ Route::get('/', function()
 
 Route::post('search','SearchController@showSearchResults');
 
-Route::get('private', array('before' => 'auth', function()
-{
-	return 'Authenticated!';
-}));
-
 Route::get('imgupload', function()
 {
 	return View::make('imgupload')->with('message', Session::get('message'));
@@ -42,3 +37,8 @@ Route::get('serveImage', 'ImageController@ServeImage');
 
 Route::get('restaurants/{id}/{name}', 'RestaurantInstanceController@showRestaurant')
     ->where(array('id' => '[0-9]+'));
+
+Route::get('tempOpinion', function(){ return csrf_token(); });
+//this route requires a parameter _token with the value of csrf_token() to work (e.g. updateReview?food_instance_id=10&rating=3&_token=asdfasdfasdfasdf )
+Route::get('updateReview', array('before' => 'auth|csrf', 'uses' => 'ReviewController@UpdateReview'));
+Route::get('getReview', 'ReviewController@GetReview');
