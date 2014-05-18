b5cbb8cdc33e98d8a68a5a5eae3b293de18aba4e
Nathan Chou <starrynte@gmail.com>
2014-05-18 01:34:44 -0700

    Tiny fix to ImageController. NOTE: If food_instance_food_type_id is renamed to food_instance_food_types_id in the food_images table, please change line 25 correspondingly.

--- app/controllers/ImageController.php
+++ app/controllers/ImageController.php
@@ -20,9 +20,9 @@ class ImageController extends BaseController
 			{
 				$food_instances_id = Input::get('food_instances_id');
 				$food_instance = food_instance::find($food_instances_id);
-				$image = new Food_Image;
+				$image = new FoodImage;
 				$image->food_instances_id = $food_instances_id;
-				$image->food_instances_food_types_id = $food_instance->food_types_id;
+				$image->food_instances_food_type_id = $food_instance->food_types_id; //TODO this really should be food_types_id, but this is just to match the database..
 				$image->food_instances_restaurants_id = $food_instance->restaurants_id;
 				$image->imageData = base64_encode(File::get($file));
 				$image->save();
